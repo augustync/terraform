f3ab06864cede40bf4856e68468556188712c38a
Augustyn Chmiel <augustyn.chmiel@acoustic.com>
2021-03-14 17:10:17 +0000

    create s3 bucket for tf

---
+++ sandbox/ft_code_s3.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+    profile = var.profile
+    region = var.region
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-2"
+}
+
+variable "profile" {
+  type    = string
+  default = "Sandbox_InfraDeployer"
+}
+
+
+resource "aws_s3_bucket" "s3" {
+  bucket = "auggie-sandbox-tf-tests"
+  key    = "auggie-sandbox/tf.tests"
+  ac1    = "private"
+}
